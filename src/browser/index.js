979634dabd5f8cc4692c671baf1184942f06c4c2
Damien Maillard <damien.maillard@dailymotion.com>
2018-10-17 19:24:30 +0200

    Allow 300 statuses, do not rewrite sourceMappingURL

--- src/browser/index.js
+++ src/browser/index.js
@@ -4,21 +4,11 @@ const browserSystem = new window.System.constructor()
 
 browserSystem.instantiate = (url, parent) => {
   return fetchUsingXHR(url).then(({ status, headers, reason, body }) => {
-    if (status < 200 || status >= 300) {
+    if (status < 200 || status >= 400) {
       return Promise.reject({ status, reason, headers, body })
     }
 
-    const sourceMapName = headers["x-sourcemap-name"]
-    const sourceLocation = headers["x-location"]
-    const lastSlashIndex = sourceLocation.lastIndexOf("/")
-    const sourceDirname = location.slice(
-      0,
-      lastSlashIndex === -1 ? sourceLocation.length : lastSlashIndex,
-    )
-    const sourceMapLocation = `${sourceDirname}/${sourceMapName}`
-
     body = `${body}
-${"//#"} sourceMappingURL=${sourceMapLocation}
 ${"//#"} sourceURL=${url}`
 
     try {
