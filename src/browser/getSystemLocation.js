8be3309c9f08b123ba731a077ca417b5a6692fa7
Damien Maillard <damien.maillard@dailymotion.com>
2018-10-17 19:24:06 +0200

    fix getSystemLocation

--- src/browser/getSystemLocation.js
+++ src/browser/getSystemLocation.js
@@ -1,7 +1,3 @@
-import path from "path"
-
-const root = path.resolve(__dirname, "../../../")
-
 export const getSystemLocation = () => {
-  return `${root}/node_modules/systemjs/dist/system.js`
+  return require.resolve("systemjs/dist/system.js")
 }
