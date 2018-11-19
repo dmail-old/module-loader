7909237883ff8386d7518c88ab6e1dc3ad0f78c9
Damien Maillard <damien.maillard@dailymotion.com>
2018-11-19 15:03:35 +0100

    prepare compilation

--- script/compile-browser-system.js
+++ script/compile-browser-system.js
@@ -1,13 +1,74 @@
-const { compileBrowserSystem } = require("../dist/index.js")
-// TODO, import this from '@dmail/helper'
-// const { fileSystemWriteCompileResult } = require('@dmail/helper')
-const { fileSystemWriteCompileResult } = require("./fileSystemWriteCompileResult.js")
+const {
+  pluginOptionMapToPluginMap,
+  pluginMapToPluginsForPlatform,
+  fileSystemWriteCompileResult,
+} = require("@dmail/project-structure-compile-babel")
 const path = require("path")
+const { rollup } = require("rollup")
+const babel = require("rollup-plugin-babel")
+const nodeResolve = require("rollup-plugin-node-resolve")
 
 const localRoot = path.resolve(__dirname, "../")
-
-debugger
-compileBrowserSystem().then((compileResult) => {
-  debugger
-  fileSystemWriteCompileResult(compileResult, `${localRoot}/dist/browser-system.js`)
+const inputFile = `${localRoot}/src/system/browser/index.js`
+const pluginMap = pluginOptionMapToPluginMap({
+  "proposal-async-generator-functions": {},
+  "proposal-json-strings": {},
+  "proposal-object-rest-spread": {},
+  "proposal-optional-catch-binding": {},
+  "proposal-unicode-property-regex": {},
+  "transform-arrow-functions": {},
+  "transform-async-to-generator": {},
+  "transform-block-scoped-functions": {},
+  "transform-block-scoping": {},
+  "transform-classes": {},
+  "transform-computed-properties": {},
+  "transform-destructuring": {},
+  "transform-dotall-regex": {},
+  "transform-duplicate-keys": {},
+  "transform-exponentiation-operator": {},
+  "transform-for-of": {},
+  "transform-function-name": {},
+  "transform-literals": {},
+  "transform-new-target": {},
+  "transform-object-super": {},
+  "transform-parameters": {},
+  "transform-regenerator": {},
+  "transform-shorthand-properties": {},
+  "transform-spread": {},
+  "transform-sticky-regex": {},
+  "transform-template-literals": {},
+  "transform-typeof-symbol": {},
+  "transform-unicode-regex": {},
 })
+
+const compile = async () => {
+  const plugins = pluginMapToPluginsForPlatform(pluginMap, "unknown", "0.0.0")
+
+  const bundle = await rollup({
+    input: inputFile,
+    plugins: [
+      nodeResolve({
+        module: true,
+      }),
+      babel({
+        babelrc: false,
+        exclude: "node_modules/**",
+        plugins,
+      }),
+    ], // comment line below to skip rollup warnings
+    // onwarn: () => {},
+  })
+
+  const compileResult = bundle.generate({
+    format: "iife",
+    name: "__browserSystem__",
+    sourcemap: true,
+  })
+
+  debugger
+
+  await fileSystemWriteCompileResult(compileResult, `browser-system.js`, `${localRoot}/dist`)
+  console.log(`${inputFile} -> dist/browser-system.js`)
+}
+
+compile()
